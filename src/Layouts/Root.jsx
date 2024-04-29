5f628954824d0a54facb5ef55c3d9a01f01577f7
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-04-29 10:49:28 +0800

    hero added and make it dynamic

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Pages/Nav";
+import Home from "../Pages/Home/Home";
 
 
 const Root = () => {
     return (
         <div>
             <Nav></Nav>
+            <Home></Home>
             <Outlet></Outlet>
         </div>
     );
